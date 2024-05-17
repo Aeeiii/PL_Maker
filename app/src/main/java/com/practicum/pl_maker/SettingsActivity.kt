65c691fc363312dca3e02220e371dc0046147085
Aeeiii <ice.king.13@yandex.ru>
2024-05-17 12:35:22 +0300

    1

---
+++ app/src/main/java/com/practicum/pl_maker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.practicum.pl_maker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val image = findViewById<ImageView>(R.id.back_button)
+
+        image.setOnClickListener {finish()
+        }
+    }
+}
