369da64416e511b7782882f2b0e21fa85cead383
Aeeiii <ice.king.13@yandex.ru>
2024-05-17 21:31:21 +0300

    1

--- app/src/main/java/com/practicum/pl_maker/MainActivity.kt
+++ app/src/main/java/com/practicum/pl_maker/MainActivity.kt
@@ -10,9 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         supportActionBar?.setDisplayShowHomeEnabled(true)
-
 
         val buttonSearch = findViewById<Button>(R.id.button_search)
         val buttonMedia = findViewById<Button>(R.id.button_media)
@@ -36,7 +34,5 @@ class MainActivity : AppCompatActivity() {
             val displayIntent = Intent(this, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
-
-
     }
 }
